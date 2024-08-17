98108c037ed4f0eae32fee6307258b02c8c7a01a
Guy Lin <166591803+guychienll-kkday@users.noreply.github.com>
2024-08-17 23:27:41 +0800

    Revert "feat: add font style for site"
    This reverts commit 56f46212985f6f88d2d2ffb09beff8d7a4c2ef3e.

--- pages/_document.js
+++ pages/_document.js
@@ -27,20 +27,6 @@ export default function Document() {
                 <meta name="twitter:title" content={title} />
                 <meta name="twitter:description" content={description} />
                 <meta name="twitter:image" content="/line_logo.png" />
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link
-                    rel="preconnect"
-                    href="https://fonts.gstatic.com"
-                    crossOrigin="true"
-                />
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-                    rel="stylesheet"
-                />
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-                    rel="stylesheet"
-                />
             </Head>
             <body>
                 <NextUIProvider>
